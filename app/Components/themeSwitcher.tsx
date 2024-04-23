b219e8c0004e81ee1606ea253594db62dcccce22
vinicius <viniciuscoelho1995@yahoo.com.br>
2024-04-23 17:41:03 -0300

    feature: added theme switcher

---
+++ app/Components/themeSwitcher.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import { Sun, Moon, SunMoon } from 'tabler-icons-react'
+import { useState, useEffect } from 'react'
+import { useTheme } from 'next-themes'
+
+export default function ThemeSwitcher() {
+  const [mounted, setMounted] = useState(false)
+  const { setTheme, resolvedTheme } = useTheme()
+
+  useEffect(() =>  setMounted(true), [])
+
+  if (!mounted) return (
+    <SunMoon/>
+  )
+
+  if (resolvedTheme === 'dark') {
+    return <Moon onClick={() => setTheme('light')} />
+  }
+
+  if (resolvedTheme === 'light') {
+    return <Sun onClick={() => setTheme('dark')} />
+  }
+
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="min-h-screen flex flex-col gap-1 items-center">
+    <main >
       <h1>Hi, this is the first version of my portfolio website!</h1>
       <h2>Soon i will have a good presentation of how i like to work</h2>
       <h3>Hope it will impress!</h3>
